586e33880410e63d76cb832d143bcb234e013270
Vrej17 <gevorgyanvrej096@gmail.com>
2024-05-17 21:30:16 +0400

    :q

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import CardApp from "./App";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
-  { path: "/cardgame", element: <CardApp />, errorElement: <div>err</div> },
+  { path: "/", element: <CardApp />, errorElement: <div>err</div> },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
